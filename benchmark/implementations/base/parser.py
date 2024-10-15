c1b46772501835ef44935a6b5bc52d6b7a581eff
AlexandraImbrisca <alexandra.imbrisca6@gmail.com>
2024-10-15 20:52:55 +0200

    Move base implementation to default functions

--- benchmark/implementations/base/parser.py
+++ benchmark/implementations/base/parser.py
@@ -2,18 +2,13 @@ from typing import Literal
 from zipfile import ZipFile
 
 import pandas as pd
-from lxml.etree import XMLSyntaxError
 from sqlalchemy.engine import Engine
-from sqlalchemy.exc import DataError, IntegrityError
 
 from benchmark.implementations.skeleton.parser import ParserSkeleton
 from benchmark.implementations.skeleton.utilities import (
-    add_missing_columns_to_table,
-    delete_wrong_xml_entry,
-    handle_xml_syntax_error,
-    write_single_entries_until_not_unique_comes_up,
+    default_add_table_to_database,
+    default_read_xml,
 )
-from open_mastr.utils.orm import tablename_mapping
 
 
 class Parser(ParserSkeleton):
@@ -25,43 +20,7 @@ class Parser(ParserSkeleton):
             if_exists: Literal["fail", "replace", "append"],
             engine: Engine,
     ) -> None:
-        # get a dictionary for the data types
-        table_columns_list = list(
-            tablename_mapping[xml_table_name]["__class__"].__table__.columns
-        )
-        dtypes_for_writing_sql = {
-            column.name: column.type
-            for column in table_columns_list
-            if column.name in df.columns
-        }
-
-        add_missing_columns_to_table(engine, xml_table_name, column_list=df.columns.tolist())
-
-        for _ in range(10000):
-            try:
-                with engine.connect() as con:
-                    with con.begin():
-                        df.to_sql(
-                            sql_table_name,
-                            con=con,
-                            index=False,
-                            if_exists=if_exists,
-                            dtype=dtypes_for_writing_sql,
-                        )
-                        break
-
-            except DataError as err:
-                delete_wrong_xml_entry(err, df)
-
-            except IntegrityError:
-                # error resulting from Unique constraint failed
-                df = write_single_entries_until_not_unique_comes_up(
-                    df, xml_table_name, engine
-                )
+        default_add_table_to_database(df, xml_table_name, sql_table_name, if_exists, engine)
 
     def read_xml(self, f: ZipFile, file_name: str) -> pd.DataFrame:
-        data = f.read(file_name)
-        try:
-            return pd.read_xml(data, encoding="UTF-16", compression="zip")
-        except XMLSyntaxError as error:
-            return handle_xml_syntax_error(data.decode("utf-16"), error)
+        return default_read_xml(f, file_name)
